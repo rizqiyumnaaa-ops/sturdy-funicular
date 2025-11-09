2e04df5d0624078671faaba99af3786bbc7f6673
rizqiyumnaaa-ops <rizqiyumnaaa@gmail.com>
2025-11-09 12:45:01 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Aktifkan AOS (Animate On Scroll)
+AOS.init({
+    duration: 1200, // Durasi animasi dalam milidetik (1.2 detik)
+    once: true      // Animasi hanya berjalan sekali per elemen
+});
